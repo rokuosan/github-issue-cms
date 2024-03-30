bd4733a738b1d04af56082872de8040447e71cd0
rokuosan <85651386+rokuosan@users.noreply.github.com>
2024-03-30 17:08:10 +0900

    feat: Exports an article as markdown

--- pkg/converter/article.go
+++ pkg/converter/article.go
@@ -1,5 +1,15 @@
 package converter
 
+import (
+	"bufio"
+	"fmt"
+	"github.com/spf13/viper"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+)
+
 // Article is the article for Hugo.
 type Article struct {
 	// Author is the author of the article.
@@ -27,4 +37,84 @@ type Article struct {
 	// ExtraFrontMatter is the extra front matter of the article.
 	// It must be a valid YAML string.
 	ExtraFrontMatter string `json:"extra_front_matter"`
+
+	Key string
+}
+
+func (article *Article) ExportToMarkdown(name string) {
+	// Get export directory
+	articlesDir := viper.GetString("hugo.directory.articles")
+	if articlesDir == "" {
+		articlesDir = "content/posts"
+	}
+	// Sanitize
+	articlesDir = strings.ReplaceAll(articlesDir, ".", "")
+	if strings.HasPrefix(articlesDir, "/") {
+		re := regexp.MustCompile("^/+")
+		match := re.FindString(articlesDir)
+		articlesDir = strings.Replace(articlesDir, match, "", 1)
+	}
+	if strings.HasSuffix(articlesDir, "/") {
+		re := regexp.MustCompile("/+$")
+		match := re.FindString(articlesDir)
+		articlesDir = strings.Replace(articlesDir, match, "", 1)
+	}
+
+	// Create directory
+	path := filepath.Join(strings.Split(articlesDir, "/")...)
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		if err := os.MkdirAll(path, 0777); err != nil {
+			panic(err)
+		}
+	}
+
+	// Create file
+	path = filepath.Join(path, name+".md")
+	file, err := os.Create(path)
+	if err != nil {
+		panic(err)
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(file)
+
+	// Build String
+	var content []byte
+	var tags []byte
+	for _, tag := range article.Tags {
+		tags = append(tags, "  - "...)
+		tags = append(tags, tag...)
+		tags = append(tags, '\n')
+	}
+	rawText := strings.TrimSpace(fmt.Sprintf(`---
+title: %s
+author: %s
+date: %s
+draft: %t
+categories: 
+  - %s
+tags:
+%s
+%s
+---
+
+%s
+`,
+		article.Title,
+		article.Author,
+		article.Date,
+		article.Draft,
+		article.Category,
+		tags,
+		article.ExtraFrontMatter,
+		article.Content))
+	content = append(content, rawText...)
+
+	// Write
+	writer := bufio.NewWriter(file)
+	_, _ = writer.Write(content)
+	_ = writer.Flush()
 }
